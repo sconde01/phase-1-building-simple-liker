eebbeb9c9eedcfa10101f61d68e0a891d6ff5a81
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-05-03 20:36:10 -0700

    commit tree b3126ebdf2e7773b2183ca59dbf21de3cc417f77

--- main.js
+++ main.js
@@ -2,7 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+FULL_HEART.class = '.activated-heart'
+console.log(FULL_HEART)
+
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+
+  const hearts = document.querySelectorAll("span.like-glyph")
+  console.log(hearts)
+
+  //hearts.forEach(hearts => hearts.className= "activate-heart"
+  hearts.forEach(hearts => hearts.addEventListener("click", likeCallback))
+
+  function likeCallback(hearts){
+    console.log (hearts.target)
+    mimicServerCall()
+    .then(() =>{
+      if (hearts.target.innerText === EMPTY_HEART){
+        hearts.target.innerText = FULL_HEART
+        //hearts.classList.add("activated-heart")
+      }
+      else if (hearts.target.innerText ===FULL_HEART){
+        hearts.target.innerText = EMPTY_HEART
+      }
+    })
+    .catch(() => {
+      const errorMessage = document.getElementById("modal")
+      console.log(errorMessage)
+      errorMessage.className = "show"
+
+      setTimeout(() => {
+        const errorMessage = document.getElementById("modal")
+        console.log(errorMessage)
+        errorMessage.className = "hidden"}, 3000)}
+      )}
+    })
+  
 
 
 
